726fd509b28c9f4e120e610b097f4e476fc82a44
Simon D. Levy <simon.d.levy@gmail.com>
2014-12-31 02:13:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ This code is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU Lesser General Public License
 along with this code. If not, see <http://www.gnu.org/licenses/>.
 '''
